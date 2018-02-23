2f28c6355ca6216d3913343a639b9b0d82722b17
Andrew Young <ajyounguk@users.noreply.github.com>
2018-02-23 17:53:36 +0000

    added more s3 functionality

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
+// AWS S3 API demo
 
-
-// AWS test SNS server / API harness
 'use strict'
 
 // load the AWS SDK
 var aws         = require('aws-sdk')
 var express     = require('express');
 var app         = express();
-const uuidv5    = require('uuid/v5');
 var fs          = require('fs')
 
 // serve up pages from /public
@@ -18,11 +16,10 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-
 // load aws config
 aws.config.loadFromPath(__dirname + '/config.json')
 
-// create the sqs service object
+// create the aws service object
 var s3 = new aws.S3()
 
 // serve up main test page 
@@ -41,11 +38,10 @@ app.post('/bucket', function (req, res) {
     }
  
     s3.createBucket(params, function(err, data) {
-      
         if (err) {
             console.log('!!!(create bucket) Error creating bucket', err)
             res.status(500)
-            res.end()
+            res.send(err)
         } else {
             console.log('(create bucket) Created bucket', req.body.bucketname)
             res.status(200)
@@ -56,7 +52,24 @@ app.post('/bucket', function (req, res) {
 })
 
 
-// 2. Upload (local) file
+// 2. List Buckets
+
+app.get('/bucket', function (req, res){
+
+    s3.listBuckets(function(err, data) {
+        if (err) {
+            console.log('!!!(list  bucket) Error', err)
+            res.status(500)
+            res.send(err)
+        } else {
+            console.log('(list buckets) returns:', data)
+            res.status(200)
+            res.send(data)
+        }
+     })
+})
+
+// 3. Upload (local) file
 app.post('/bucket/file', function  (req, res) {
 
     var params = {
@@ -66,249 +79,87 @@ app.post('/bucket/file', function  (req, res) {
 
     filestream.on('error', function (err) {
         console.log('!!!!(upload file) Error',err)
+        res.status(500)
+        res.end()
     })
 
     params.Body = filestream
     params.Key = req.body.filename
 
     s3.upload (params, function (err, data) {
-
         if (err) {
-            console.log('!!!(upload to bucket) Error uploading file', err)
+            console.log('!!!(upload file) Error uploading file', err)
             res.status(500)
-            res.end()
+            res.send(err)
         } else {
-            console.log('(uploda to bucket) File', req.body.filename, 'uploaded to s3')
+            console.log('(upload file) File', req.body.filename, 'uploaded to s3')
             res.status(200)
             res.send(data)
         }
-
     })
 })
 
 
+// 4. List Objects in Bucket
+app.get('/bucket/objects', function (req, res) {
 
-
-/* // CODE TO BE DELETED - FOR REFERENCE ONLY
-// 1. create Topic - input = topic name
-app.post('/sns', function (req, res) {
- 
-    var snsParams = {
-        Name: req.body.snstopic
-    }
-
-    sns.createTopic(snsParams, function(err, data) {
-        if (err) {
-            console.log("!!(create topic) Error creating topic", err)
-            res.status(500)
-            res.send(err)
-        } else {
-            console.log("(create topic) topic\" " + req.body.snstopic + " \"created")
-            console.log("(create topic) topic arn is:", data.TopicArn)
-            res.status(201)
-            res.send(data)
-        }
-    })
-})
-
-// 2. subscribe queue to SNS topic
-app.post('/sns/subscribe-queue', function (req, res) {
- 
-    var snsParams = {
-        'TopicArn': req.body.snsarn,
-        'Protocol': 'sqs',
-        'Endpoint': req.body.sqsarn
-
-    }
-
-    sns.subscribe(snsParams, function(err, data) {
-        if (err) {
-            console.log("!!(subscribe queue) Error", err)
-            res.status(500)
-            res.send(err)
-        } else {
-            console.log("(subscribe queue) subscribed queue", req.body.sqsarn, "to topic" ,req.body.snsarn)
-            res.status(201)
-            res.send(data)
-        }
-    })
-})
-
-// 3. subscribe email to SNS topic
-app.post('/sns/subscribe-email', function (req, res) {
- 
-    var snsParams = {
-        'TopicArn': req.body.snsarn,
-        'Protocol': 'email',
-        'Endpoint': req.body.email
-
-    }
-
-    sns.subscribe(snsParams, function(err, data) {
-        if (err) {
-            console.log("!!(subscribe email) Error", err)
-            res.status(500)
-            res.send(err)
-        } else {
-            console.log("(subscribe email) subscribed email", req.body.email, "to topic" ,req.body.snsarn)
-            res.status(201)
-            res.send(data)
-        }
-    })
-})
-
-// 4. Send message to SNS topics
-app.post('/sns/message', function (req, res) {
-
-    var snsParams = {
-        Message: req.body.snsmessage,
-        TopicArn: req.body.snstopicarn
-    }
-
-    sns.publish (snsParams, function (err, data) {
-        if (err) {
-            console.log("!!(sns publish) Error", err)
-            res.status(500)
-            res.send(err)
-        } else {
-            console.log("(sns publish) sent message:", req.body.snsmessage)
-            console.log("to topic arn:", req.body.snstopicarn)
-            res.status(200)
-            res.send(data)
-        }
-    })   
-})
-
-// 5. List SNS topics
-app.get('/sns', function (req, res) {
-
-    function listone (listNextToken) {
-
-        var datalist = { set: [] }
-        var i = 0
-
-        if (listNextToken) {
-            var snsParams = { NextToken : listNextToken }
-            console.log("(sns list topics) >>> iterating, next token is:", listNextToken)
-        } else {
-            var snsParams = {}
-        }
-
-        sns.listTopics (snsParams, function (err, data) {
-            if (err) {
-                console.log("!!(sns list topics) Error", err)
-                res.status(500)
-                res.send(err)
-            } else {
-                console.log("(sns list topics) Topics:")
-                datalist.set[i] = data
-                i++
-                console.log(data)
-
-                // if we have a next token, list again
-                if (data.NextToken) {
-                    setTimeout(listone(data.NextToken),0)
-                } else {
-                    res.status(200)
-                    res.send(datalist)
-                }
-            }
-        })
-    }
-    listone()
-})
-
-// 6. Delete Topic
-app.post('/sns/delete-topic', function (req, res) {
-
-    var snsParams = {
-        TopicArn: req.body.snsarn
+    var params = {
+        Bucket : req.query.bucketname
     } 
 
-    sns.deleteTopic (snsParams, function (err, data) {
+    s3.listObjects(params, function(err, data) {
         if (err) {
-            console.log("!!(sns delete topic) Error", err)
+            console.log('!!!(list objects) Error listing objects', err)
             res.status(500)
             res.send(err)
         } else {
-            console.log("(sns delete topic) deleted topic for arn:", req.body.snsarn)
+            console.log('(list objects) Objects in bucket', req.query.bucketname +':')
             res.status(200)
             res.send(data)
         }
-    })   
+    })
 })
 
+// 5. Delete object
+app.post('/bucket/file/delete', function (req, res) {
 
-// 7. List SNS subscriptions
-app.get('/sns/subscription', function (req, res) {
-
-    var datalist = { set: [] }
-    var i = 0
-
-    function listone (listNextToken) {
-  
-        if (listNextToken) {
-            var snsParams = { NextToken : listNextToken }
-            console.log("(sns list subscriptions) >>> iterating, next token is:", listNextToken)
-        } else {
-            var snsParams = {}
-        }
-        
-        sns.listSubscriptions (snsParams, function (err, data) {
-            if (err) {
-                console.log("!!(sns list subscriptions) Error", err)
-                res.status(500)
-                res.send(err)
-            } else {
-                console.log("(sns list subscriptions) Subscriptions:")
-                datalist.set[i] = data
-                i++
-                console.log(data)
-
-                // if we have a next token, list again
-                if (data.NextToken) {
-                    listNextToken = data.NextToken
-          
-                // wrap recursion with set timeout
-                setTimeout(function () {
-                    listone(data.NextToken);
-                    }, 0);
-
-                } else {
-                    res.status(200)
-                    res.send(datalist)    
-                }
-            }
-        })
+    var params = {
+        Bucket: req.body.bucketname, 
+        Key: req.body.filename
     }
-    listone()
-})
 
-/
-
-// 8. Unsubscribe
-app.post('/sns/delete-subscription', function (req, res) {
-
-    var snsParams = {
-        SubscriptionArn: req.body.snssubarn
-    } 
-
-    sns.unsubscribe (snsParams, function (err, data) {
+    s3.deleteObject(params, function(err, data) {
         if (err) {
-            console.log("!!(sns delete subscription) Error", err)
+            console.log('!!!(delete object) Error deleting object', err)
             res.status(500)
             res.send(err)
         } else {
-            console.log("(sns delete subscription) deleted subscription  arn:", req.body.snssubarn)
+            console.log('(delete object) Object', req.body.filename,'in bucket', req.body.bucketname, 'deleted')
             res.status(200)
             res.send(data)
         }
-    })   
-
-
+   })
 })
 
-*/
+// 6. Delete Bucket
+app.post('/bucket/delete', function (req, res) {
+
+    var params = {
+        Bucket: req.body.bucketname
+    }
+
+    s3.deleteBucket(params, function(err, data) {
+        if (err) {
+            console.log('!!!(delete bucket) Error deleting object', err)
+            res.status(500)
+            res.send(err)
+        } else {
+            console.log('(delete bucket) Bucket', req.query.bucketname, 'deleted')
+            res.status(200)
+            res.send(data)
+        }
+   })
+})
 
 function start_webserver(port) { 
 
